f75604108e6317fe34b21d4eeb41baf136c54700
Student <student@sch239.net>
2020-10-10 18:51:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/simpledraw/MyView.java
@@ -0,0 +1,21 @@
+package com.example.simpledraw;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.View;
+
+public class MyView extends View {
+
+    public MyView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        canvas.drawLine(0, 0, canvas.getWidth(),canvas.getHeight(),paint);
+
+    }
+}
